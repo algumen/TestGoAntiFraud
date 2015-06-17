688cf09bc3e2ea0a7b2eafae80ae8d0b4ec39532
algumen <algumen@gmail.com>
2015-06-17 07:43:15 +0300

    first

--- Tests/src/main/java/Tests/TestCorrectEnter.java
+++ Tests/src/main/java/Tests/TestCorrectEnter.java
@@ -1,19 +1,37 @@
 package Tests;
 
+import Pages.LoginPage;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestCorrectEnter {
 
-    private final By loginButton = By.id("login");
-    driver.findElement(By.id("login")).clear();
+    private WebDriver driver;
+    private String baseUrl;
+    private final username = "tt";
 
+
+//    private final By loginButton = By.id("login");
+//    driver.findElement(By.id("login")).clear();
+    @Before
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
         baseUrl = "https://goantifraud.com/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
+    @Test
+    public void testUntitled() throws Exception {
+        driver.get(baseUrl + "/manager/login/");
+        LoginPage.typeUsername("tt");
+
+
+    }
+
+
 
 
 
