fd31416bfe2a0291b98db4a09e63958785876232
algumen <algumen@gmail.com>
2015-06-18 07:04:06 +0300

    final 1 test

---
+++ Tests/src/main/java/Tests/TestIncorrectEnter.java
@@ -0,0 +1,51 @@
+package Tests;
+
+import Pages.LoginPage;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestIncorrectEnter {
+
+    private WebDriver driver;
+    private final String username = "ttt";
+    private final String password = "123456";
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testCorrectLogin() throws Exception {
+        driver.get("https://goantifraud.com/manager/");
+
+        LoginPage login = new LoginPage(driver);
+        login.loginAs(username, password);
+
+        Assert.assertTrue(isElementPresent(By.xpath(".//*/div[contains(text(),'Login or password is incorrect')]")));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
